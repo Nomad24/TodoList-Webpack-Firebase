3f71ecc3882487ab7bcdd6386877e8b2495138e4
Nomad24 <36933956+Nomad24@users.noreply.github.com>
2019-09-02 15:43:18 +0300

    Delete transform.service.js

--- components/transform.service.js
+++
@@ -1,9 +0,0 @@
-export class TransformService {
-    static fbObjectToArray(fbData) {
-        return Object.keys(fbData).map(function(key) {
-            const item = fbData[key]
-            item.id = key
-            return item
-        })
-    }
-}
